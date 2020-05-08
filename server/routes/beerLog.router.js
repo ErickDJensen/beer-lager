8748e4a0362a50ea38a0c8a8982a4e65c93d3d80
Erick Jensen <Erick.Jensen010@gmail.com>
2020-05-08 11:49:57 -0500

    change beer log to display ordered by date

--- server/routes/beerLog.router.js
+++ server/routes/beerLog.router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
     console.log('Beer log GET arrived at server', req.user.id);
     let param = req.user.id;
-    pool.query(`SELECT * FROM "user_input" WHERE "user_id" = $1`, [param])
+    pool.query(`SELECT * FROM "user_input" WHERE "user_id" = $1 ORDER BY "date"`, [param])
         .then((result) => {
             res.send(result.rows);
         })
